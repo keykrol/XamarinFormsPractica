fd4434f48732b9d891f5fe22d0525bb3b860ea6a
desarrollador9 <desarrollador9@softcapla.com>
2021-04-08 11:16:17 +0200

    IvalueConverter Practica Listando colores

---
+++ XamarinPruebaMoneda/PruebaMoneda/PruebaMoneda/PruebaMoneda/NumberToColorConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace PruebaMoneda
+{
+	public class NumberToColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string color = "";
+			int colorNumber = (int)value;
+
+			switch (colorNumber)
+			{
+				case 1:
+					color = "#ed0707";
+					break;
+
+				case 2:
+					color = "#34db25";
+					break;
+
+				case 3:
+					color = "#fcd46d";
+					break;
+
+				case 4:
+					color = "#2e15eb";
+					break;
+
+				case 5:
+					color = "#0ff2e3";
+					break;
+
+				default:
+					color = "#000000";
+					break;
+			}
+
+			return color;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+
+
+}
---
+++ XamarinPruebaMoneda/PruebaMoneda/PruebaMoneda/PruebaMoneda/PruebaMoneda.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PruebaMoneda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PruebaMoneda : ContentPage
+    {
+        public PruebaMoneda()
+        {
+            InitializeComponent();
+
+            List<Data> myData = new List<Data>();
+            myData.Add(new Data { Name = "this is red color", ColorNumber = 1 });
+            myData.Add(new Data { Name = "this is green color", ColorNumber = 2 });
+            myData.Add(new Data { Name = "this is orange color", ColorNumber = 3 });
+            myData.Add(new Data { Name = "this is blue color", ColorNumber = 4 });
+            myData.Add(new Data { Name = "this is violate color", ColorNumber = 5 });
+
+            listData.ItemsSource = myData;
+        }
+
+    }
+    public class Data
+    {
+
+        public string Name { get; set; }
+        public int ColorNumber { get; set; }
+    }
+}
